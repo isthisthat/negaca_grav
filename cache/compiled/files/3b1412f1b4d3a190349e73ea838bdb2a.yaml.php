b8019c7d09fd33314b927c21f51718fc7d49e208
Stathis <skanterakis@gmail.com>
2016-08-22 12:13:24 +0100

    removed cache and logs

--- cache/compiled/files/3b1412f1b4d3a190349e73ea838bdb2a.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'themes://bootgrav/bootgrav.yaml',
-    'modified' => 1471733123,
-    'data' => [
-        'enabled' => true
-    ]
-];
